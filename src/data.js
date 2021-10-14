a76bcc254f769966f6cf70c76ab2cf6a2434ea9d
nochi <marcocesana98@gmail.com>
2021-10-14 17:21:33 +0200

    First commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    title: "Scopus4FP7",
+    subtitle: "Web application",
+    description:
+      "The Scopus4FP7 Web application allows accredited users to consult the data relating to the scientific production inherent in the research projects of the EU-FP7 program. The application concerns both the projects presented and funded (beneficiary projects), and the projects presented and not funded (non-beneficiaries).",
+    image: "./project-1.gif",
+    link: "https://github.com/baltornat/scopus4fp7",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
