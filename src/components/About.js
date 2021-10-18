c2d0eb7761104e6e9082be225f652f9144fa9c2a
nochi <marcocesana98@gmail.com>
2021-10-18 21:26:03 +0200

    Radical changes

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,12 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
             Hi, I'm Marco.
-            <br className="hidden lg:inline-block" />I am a student at the University of Milan.
+            <br className="hidden lg:inline-block" />I am currently a master's degree student in cyber security at the University of Milan.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            I was born in Italy near Milan in 1998. I have always been intrigued by the world of computers, in fact I studied computer science in both high school and university. I graduated in computer science in 2021 and I'm currently attending a master's degree program in cyber security.
           </p>
           <div className="flex justify-center">
             <a
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
             Marco Cesana
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center text-white">
           <a href="#projects" className="mr-5 hover:text-white">
             Past Work
           </a>
@@ -20,8 +20,8 @@ export default function Navbar() {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Hire Me
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-white">
+          Contact Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
