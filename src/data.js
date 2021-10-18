0ceb2f525a05ed1f0dbbe2321afa28614c1df7ae
nochi <marcocesana98@gmail.com>
2021-10-18 20:37:39 +0200

    Changed link and images

--- src/data.js
+++ src/data.js
@@ -4,6 +4,7 @@ export const projects = [
     subtitle: "Web application",
     description:
       "The Scopus4FP7 Web application allows accredited users to consult the data relating to the scientific production inherent in the research projects of the EU-FP7 program. The application concerns both the projects presented and funded (beneficiary projects), and the projects presented and not funded (non-beneficiaries).",
+    image: "eu_flag.jpg",  
     link: "https://github.com/baltornat/scopus4fp7",
   },
   {
@@ -11,7 +12,8 @@ export const projects = [
     subtitle: "Python dashboard",
     description:
       "We observed the pollution levels in Lombardia (Italy) from 2006 to 2018. We decided to study both the relation between pollution and weather conditions and the relation between pollution and deseases. We made a dashboard using Plotly, a powerful Python module.",
-    link: "https://github.com/baltornat/scopus4fp7",
+    image: "undraw_environment_iaus.png",  
+    link: "https://github.com/prometheus404/sci-vis-project",
   },
 ];
 
