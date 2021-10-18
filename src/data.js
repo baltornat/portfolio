9cc6e6542962b9e257eece4aa36078d5e6e296f2
nochi <marcocesana98@gmail.com>
2021-10-18 19:58:34 +0200

    Changes

--- src/data.js
+++ src/data.js
@@ -4,16 +4,28 @@ export const projects = [
     subtitle: "Web application",
     description:
       "The Scopus4FP7 Web application allows accredited users to consult the data relating to the scientific production inherent in the research projects of the EU-FP7 program. The application concerns both the projects presented and funded (beneficiary projects), and the projects presented and not funded (non-beneficiaries).",
-    image: "./project-1.gif",
+    link: "https://github.com/baltornat/scopus4fp7",
+  },
+  {
+    title: "Air quality analysis",
+    subtitle: "Python dashboard",
+    description:
+      "We observed the pollution levels in Lombardia (Italy) from 2006 to 2018. We decided to study both the relation between pollution and weather conditions and the relation between pollution and deseases. We made a dashboard using Plotly, a powerful Python module.",
     link: "https://github.com/baltornat/scopus4fp7",
   },
 ];
 
 export const skills = [
+  "C",
+  "Python",
+  "Java",
+  "C++",
+  "CSS",
+  "HTML",
+  "PHP",
   "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+  "Assembly",
+  "Linux",
+  "Microsoft Windows",
+  "Framework Yii2",
 ];
